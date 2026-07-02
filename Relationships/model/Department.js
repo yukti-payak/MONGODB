00442888f88fd9fd0a36323c76d285f42f9f4a0c
Yukti Payak <21301064yukti@viva-technology.org>
2026-07-02 16:24:17 +0530

    Remove unnecessary files from GitHub

--- Relationships/model/Department.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const departmentSchema = new mongoose.Schema({
-  departmentName: String,
-  location: String
-});
-
-export default mongoose.model("Department", departmentSchema);
